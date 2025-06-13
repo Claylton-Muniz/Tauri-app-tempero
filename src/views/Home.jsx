c96702f0a9bf315384caf607e61cee78cdfbf29f
Claylton-Muniz <claymuniz67@gmail.com>
2025-06-12 21:40:37 -0300

    Criação de uma janela externa

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,9 +1,29 @@
+import { WebviewWindow } from '@tauri-apps/api/webviewWindow'
+
 function Home() {
+  const abrirJanelaTest = () => {
+    const webview = new WebviewWindow('teste', {
+      title: 'test',
+      url: '/#/teste',
+      width: 1100,
+      height: 620,
+      fullscreen: false
+    });
+
+    webview.once('tauri://created', function () {
+      console.log('Janela test criada');
+    });
+    
+    webview.once('tauri://error', function (e) {
+      console.error('Erro ao criar janela test:', e);
+    });
+  };
 
   return (
-    <main className="container">
+    <div>
       <h1>Página 1</h1>
-    </main>
+      <button onClick={abrirJanelaTest}>Abrir janela de teste</button>
+    </div>
   );
 }
 
